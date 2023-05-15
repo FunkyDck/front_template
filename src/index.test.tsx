0191a622a6a46d98f6c5d3e8311563f3d386b609
Danila Shutov <dcbrwn2@gmail.com>
2023-05-15 21:10:33 +0300

    Add tests and enforce code style

---
+++ src/index.test.tsx
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "@jest/globals"
+
+describe("hello world", () => {
+  test("world is OK", () => {
+    expect(2 + 2).toBe(4)
+  })
+})
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client';
+import React from "react"
+import { createRoot } from "react-dom/client"
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
-
-// Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+const root = createRoot(document.getElementById("app"))
+root.render(<h1>Hello, world</h1>)
